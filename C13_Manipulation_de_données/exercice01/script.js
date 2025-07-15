c66b89763e021cf3357cfed2dae8918ac027d695
Sosow20 <sofiacherifi1995@gmail.com>
2025-07-15 10:18:31 +0200

    first commit

---
+++ C13_Manipulation_de_données/exercice01/script.js
@@ -0,0 +1,24 @@
+const candidates = ['Le Filip','Ruby On The Nail','Leona Winter','Lula Strega','Misty Phoenix',
+  'Perseo','Norma Bell','Edeha Noire','Magnetica','Afrodite Amour'];
+
+let list = document.getElementById("myList")
+
+
+
+// Etape 1
+console.log(candidates[0])
+
+
+// Etape 2
+console.log(candidates[7])
+
+// Etape 3
+console.log(candidates)
+
+// Etape 4
+
+for (let i = 0; i < candidates.length; i++) {
+    let li = document.createElement('li')
+    li.innerText = candidates[i]
+    list.appendChild(li)
+}
